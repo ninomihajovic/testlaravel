dbb1e060be9714f8a76c83e842b8132d363db1cc
ninomihajovic <41361253+ninomihajovic@users.noreply.github.com>
2018-07-18 22:50:25 +0800

    init

---
+++ app/Http/Controllers/Api/LinkedInController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Http\Resources\LinkedInResource;
+
+class LinkedInController extends Controller
+{
+    CONST HOME_URL  = 'https://www.linkedin.com/';
+    CONST LOGIN_URL = 'https://www.linkedin.com/uas/login-submit';
+    CONST LOGIN_OUT = 'https://www.linkedin.com/m/logout/';
+
+    CONST COOKIE_FILE = 'linkedin.cookie';
+
+    /**
+     * Fetch and return the linkedin api token.
+     *
+     * @param Request $info
+     */
+    public function store(Request $linkedin)
+    {
+        // Goto Login Page.
+        $html = $this->send_request(self::LOGIN_OUT);
+        $doc = new \DOMDocument();
+        @$doc->loadHTML($html);
+        $xpath = new \DomXPath($doc);
+
+        $input_elements = $xpath->query('//form[@id="login"]/input');
+
+        foreach ($input_elements as $input) {
+            $name  = $input->getAttribute('name');
+            $value = $input->getAttribute('value');
+
+            if (empty($name))
+                continue;
+
+            $form_login_params[$name] = $value;
+        }
+
+        $form_login_params['session_key']      = $linkedin->input('email');
+        $form_login_params['session_password'] = $linkedin->input('password');
+
+        $html = $this->send_request(self::LOGIN_URL, 'POST', [], $form_login_params);
+
+        // Save token file in local.
+        $cookies = $this->parse_cookies();
+        //dd($cookies);
+        \Storage::put('linkedin_token', 'token=' . $cookies['leo_auth_token']);
+
+        $data = new \stdClass();
+        $data->email = $linkedin->input('email');
+        $data->password = $linkedin->input('password');
+        $data->token = $cookies['leo_auth_token'];
+
+        $linkedin = new LinkedInResource($data);
+
+        return $linkedin;
+    }
+
+    /**
+     * Send HTTP request to site.
+     */
+    private function send_request($url, $method = 'GET', $header = [], $post_data = []) {
+        $curl = curl_init();
+
+        $curl_options = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_COOKIEJAR => storage_path(self::COOKIE_FILE),
+            CURLOPT_COOKIEFILE => storage_path(self::COOKIE_FILE)
+        ];
+
+        if (!empty($post_data)) {
+            $postvars = '';
+
+            foreach($post_data as $key => $val) {
+                $postvars .= "$key=$val&";
+            }
+
+            $curl_options[CURLOPT_POST] = true;
+            $curl_options[CURLOPT_POSTFIELDS] = $postvars;
+            $curl_options[CURLOPT_CUSTOMREQUEST] = 'POST';
+        }
+
+        curl_setopt_array($curl, $curl_options);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    private function parse_cookies() {
+        // read the file
+        $lines = file(storage_path(self::COOKIE_FILE));
+        $cookies = [];
+
+        // iterate over lines
+        foreach ($lines as $line) {
+            // we only care for valid cookie def lines
+            if ($line[0] != '#' && substr_count($line, "\t") == 6) {
+
+                // get tokens in an array
+                $tokens = explode("\t", $line);
+
+                // trim the tokens
+                $tokens = array_map('trim', $tokens);
+
+                $cookies[$tokens[5]] = $tokens[6];
+            }
+        }
+
+        return $cookies;
+    }
+}
---
+++ resources/assets/js/components/LinkedInAuthForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <form class="my-3" @submit.prevent="onSubmit">
+        <div class="form-group row">
+            <label for="linkedin_email" class="col-sm-3 col-form-label">Email address</label>
+            <div class="col-sm-9">
+                <input type="email" id="linkedin_email" name="email" v-model="linkedin.email" class="form-control" placeholder="Email address">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="linkedin_password" class="col-sm-3 col-form-label">Password</label>
+            <div class="col-sm-9">
+                <input type="password" id="linkedin_password" name="password" v-model="linkedin.password" class="form-control" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group row mt-3">
+            <label for="linkedin_password" class="col-sm-3 col-form-label">LinkedIn API token</label>
+            <div class="col-sm-9">
+                <div class="input-group mb-3">
+                    <input type="text" id="linkedin_token" class="form-control" placeholder="LinkedIn API token" aria-label="LinkedIn API token" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit">Get</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                errors: [],
+                saved: false,
+                linkedin: {
+                    email: null,
+                    password: null,
+                    data: null
+                }
+            };
+        },
+
+        methods: {
+            onSubmit() {
+                this.saved = false;
+
+                axios.post('api/linkedin/store', this.linkedin)
+                     .then(({data}) => this.setSuccessMessage())
+                     .catch(({response}) => this.setErrors(response));
+            },
+
+            setErrors(response) {
+                // this.errors = response.data.errors;
+            },
+
+            setSuccessMessage() {
+                this.reset();
+                this.saved = true;
+            },
+
+            reset() {
+                this.errors = [];
+                this.linkedin = {email: null, password: null, data: null};
+            }
+        }
+    }
+</script>
